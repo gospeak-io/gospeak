073e1cb1da5c9d119014df19229864d2061710df
Loïc Knuchel <loicknuchel@gmail.com>
2019-01-18 19:37:26 +0100

    Improve mock data

--- infra/src/main/scala/fr/gospeak/infra/services/GospeakDbInMemory.scala
+++ infra/src/main/scala/fr/gospeak/infra/services/GospeakDbInMemory.scala
@@ -9,6 +9,8 @@ import scala.concurrent.Future
 
 class GospeakDbInMemory extends GospeakDb {
   private val user1 = User.Id.generate()
+  private val user2 = User.Id.generate()
+  private val user3 = User.Id.generate()
   private val group1 = Group.Id.generate()
   private val group2 = Group.Id.generate()
   private val group3 = Group.Id.generate()
@@ -19,30 +21,47 @@ class GospeakDbInMemory extends GospeakDb {
   private val talk1 = Talk.Id.generate()
   private val talk2 = Talk.Id.generate()
   private val talk3 = Talk.Id.generate()
+  private val talk4 = Talk.Id.generate()
+  private val talk5 = Talk.Id.generate()
+  private val talk6 = Talk.Id.generate()
   private val proposal1 = Proposal.Id.generate()
+  private val proposal2 = Proposal.Id.generate()
+  private val proposal3 = Proposal.Id.generate()
+  private val proposal4 = Proposal.Id.generate()
+  private val proposal5 = Proposal.Id.generate()
 
   private val users = mutable.ArrayBuffer(
-    User(user1, "Loïc", "Knuchel", Email("loicknuchel@gmail.com")),
+    User(user1, "Demo", "User", Email("demo@mail.com")),
+    User(user2, "Speaker", "User", Email("speaker@mail.com")),
+    User(user3, "Orga", "User", Email("orga@mail.com")),
     User(User.Id.generate(), "Empty", "User", Email("empty@mail.com")))
 
   private val groups = mutable.ArrayBuffer(
-    Group(group1, Group.Slug("ht-paris"), Group.Name("HumanTalks Paris"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq(user1)),
-    Group(group2, Group.Slug("paris-js"), Group.Name("Paris.Js"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq()),
+    Group(group1, Group.Slug("ht-paris"), Group.Name("HumanTalks Paris"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq(user1, user3)),
+    Group(group2, Group.Slug("paris-js"), Group.Name("Paris.Js"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq(user3)),
     Group(group3, Group.Slug("data-gov"), Group.Name("Data governance"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq(user1)))
 
   private val events = mutable.ArrayBuffer(
-    Event(event1, Event.Slug("2019-03"), group1, Event.Name("HumanTalks Paris Mars 2019"), Some("desc"), Some("Zeenea"), Seq(proposal1), Meta(user1)),
-    Event(event2, Event.Slug("2019-04"), group1, Event.Name("HumanTalks Paris Avril 2019"), None, None, Seq(), Meta(user1)),
-    Event(event3, Event.Slug("2019-03"), group2, Event.Name("Paris.Js Avril"), None, None, Seq(), Meta(user1)),
+    Event(event1, Event.Slug("2019-03"), group1, Event.Name("HumanTalks Paris Mars 2019"), Some("desc"), Some("Zeenea"), Seq(), Meta(user1)),
+    Event(event2, Event.Slug("2019-04"), group1, Event.Name("HumanTalks Paris Avril 2019"), None, None, Seq(), Meta(user3)),
+    Event(event3, Event.Slug("2019-03"), group2, Event.Name("Paris.Js Avril"), None, None, Seq(), Meta(user3)),
     Event(event4, Event.Slug("2019-03"), group3, Event.Name("Nouveaux modèles de gouvenance"), None, None, Seq(), Meta(user1)))
 
   private val talks = mutable.ArrayBuffer(
     Talk(talk1, Talk.Slug("why-fp"), Talk.Title("Why FP"), "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.", Seq(user1), Meta(user1)),
-    Talk(talk2, Talk.Slug("scala-best-practices"), Talk.Title("Scala Best Practices"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user1), Meta(user1)),
-    Talk(talk3, Talk.Slug("nodejs-news"), Talk.Title("NodeJs news"), "Cras sit amet nibh libero, in gravida nulla..", Seq(), Meta(user1)))
+    Talk(talk2, Talk.Slug("scala-best-practices"), Talk.Title("Scala Best Practices"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user1, user2), Meta(user1)),
+    Talk(talk3, Talk.Slug("nodejs-news"), Talk.Title("NodeJs news"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user1), Meta(user1)),
+    Talk(talk4, Talk.Slug("scalajs-react"), Talk.Title("ScalaJS + React = ♥"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user2, user1), Meta(user2)),
+    Talk(talk5, Talk.Slug("gagner-1-million"), Talk.Title("Gagner 1 Million au BlackJack avec Akka"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user2), Meta(user2)),
+    Talk(talk6, Talk.Slug("demarrer-avec-spark"), Talk.Title("7 conseils pour démarrer avec Spark"), "Cras sit amet nibh libero, in gravida nulla..", Seq(user2), Meta(user2))
+  )
 
   private val proposals = mutable.ArrayBuffer(
-    Proposal(proposal1, talk1, group1, Proposal.Title("Why FP"), "temporary description"))
+    Proposal(proposal1, talk1, group1, Proposal.Title("Why FP"), "temporary description"),
+    Proposal(proposal2, talk2, group1, Proposal.Title("Scala Best Practices"), "temporary description"),
+    Proposal(proposal3, talk2, group2, Proposal.Title("Scala Best Practices"), "temporary description"),
+    Proposal(proposal4, talk3, group1, Proposal.Title("NodeJs news"), "temporary description"),
+    Proposal(proposal5, talk4, group2, Proposal.Title("ScalaJS + React = ♥"), "temporary description"))
 
   private var logged: Option[User] = users.headOption
 
