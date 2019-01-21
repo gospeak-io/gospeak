91c00b901e1b5ca0077fdb5dfd29c9c412459f04
Loïc Knuchel <loicknuchel@gmail.com>
2019-01-21 08:17:14 +0100

    Fix for parallel tests

--- infra/src/test/scala/fr/gospeak/infra/testingutils/Values.scala
+++ infra/src/test/scala/fr/gospeak/infra/testingutils/Values.scala
@@ -1,8 +1,11 @@
 package fr.gospeak.infra.testingutils
 
+import java.util.UUID
+
 import fr.gospeak.infra.services.storage.sql.{GospeakDbSql, H2}
 
 object Values {
-  val dbConf = H2("org.h2.Driver", "jdbc:h2:mem:gospeak_db;MODE=PostgreSQL;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1")
-  val db = new GospeakDbSql(dbConf)
+  def dbConf = H2("org.h2.Driver", s"jdbc:h2:mem:${UUID.randomUUID()};MODE=PostgreSQL;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1")
+
+  def db = new GospeakDbSql(dbConf)
 }
