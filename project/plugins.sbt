934f9e9bc70abc938798a61f0fdd9491f937115b
Loïc Knuchel <loicknuchel@gmail.com>
2019-02-16 14:55:20 +0100

    Upgrade Play to 2.7.0

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.21")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.15")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
 addSbtPlugin("com.codacy" % "sbt-codacy-coverage" % "2.112")
--- web/test/fr/gospeak/web/testingutils/TwirlSpec.scala
+++ web/test/fr/gospeak/web/testingutils/TwirlSpec.scala
@@ -6,17 +6,25 @@ import com.danielasfregola.randomdatagenerator.RandomDataGenerator
 import fr.gospeak.core.domain.User
 import fr.gospeak.core.testingutils.Generators._
 import fr.gospeak.web.domain.{Breadcrumb, HeaderInfo, NavLink}
-import fr.gospeak.web.routes
+import fr.gospeak.web.{GospeakApplicationLoader, routes}
 import org.scalatest.{FunSpec, Matchers}
 import play.api.i18n.{Lang, Messages}
 import play.api.mvc.{AnyContentAsEmpty, Flash}
 import play.api.test.FakeRequest
+import play.api.{ApplicationLoader, Environment}
+import play.i18n.{Messages => JavaMessages}
 
 trait TwirlSpec extends FunSpec with Matchers with RandomDataGenerator {
   protected implicit val user: User = random[User]
   protected implicit val userOpt: Option[User] = Some(user)
   protected implicit val req: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
   protected implicit val flash: Flash = req.flash
+
+  private val env = Environment.simple()
+  private val context = ApplicationLoader.Context.create(env)
+  private val loader = new GospeakApplicationLoader()
+  private val application = loader.load(context)
+
   implicit val messages: Messages = new Messages {
     override def lang: Lang = Lang(Locale.ENGLISH)
 
@@ -26,7 +34,9 @@ trait TwirlSpec extends FunSpec with Matchers with RandomDataGenerator {
 
     override def translate(key: String, args: Seq[Any]): Option[String] = Some(key)
 
-    override def isDefinedAt(key: String): Boolean = false
+    override def isDefinedAt(key: String): Boolean = true
+
+    override def asJava: JavaMessages = ???
   }
   protected val h = HeaderInfo(NavLink("Gospeak", routes.HomeCtrl.index()), Seq(), Seq())
   protected val b = Breadcrumb(Seq())
