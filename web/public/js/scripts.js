c7c44299255473dc1f1f1665879298ce8dddc8c6
m.lachkar <m.lachkar@criteo.com>
2019-05-05 14:42:33 +0200

    Remove not used dateicker from script.js

--- web/public/js/scripts.js
+++ web/public/js/scripts.js
@@ -130,24 +130,6 @@ function slugify(str) {
     });
 })();
 
-// https://uxsolutions.github.io/bootstrap-datepicker/
-(function () {
-    var defaultConfig = {
-        format: 'dd/mm/yyyy',
-        weekStart: 1,
-        maxViewMode: 2,
-        language: 'en',
-        autoclose: true,
-        todayHighlight: true,
-        toggleActive: true
-    };
-    $('input.input-date').each(function () {
-        $(this).datepicker(Object.assign({}, defaultConfig, {
-            defaultViewDate: $(this).attr('startDate')
-        }));
-    });
-})();
-
 // markdown input
 (function () {
     function fetchHtml(md) {
