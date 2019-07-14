e67b8aca1904bcb247ed8f24a8f895f24172ff95
Loïc Knuchel <loicknuchel@gmail.com>
2019-07-14 15:10:04 +0200

    Improve pureconfig reads

--- web/app/fr/gospeak/web/AppConf.scala
+++ web/app/fr/gospeak/web/AppConf.scala
@@ -11,7 +11,7 @@ import play.api.Configuration
 import play.api.mvc.Cookie.SameSite
 import pureconfig.error.{CannotConvert, ConfigReaderFailure, ConfigReaderFailures, ConvertFailure}
 import pureconfig.generic.EnumCoproductHint
-import pureconfig.{ConfigCursor, ConfigReader}
+import pureconfig.{ConfigCursor, ConfigReader, Derivation}
 
 import scala.util.{Failure, Success, Try}
 
@@ -22,39 +22,7 @@ final case class AppConf(application: ApplicationConf,
 
 object AppConf {
   def load(conf: Config): Try[AppConf] = {
-    import pureconfig.generic.semiauto._
-
-    implicit val secretConverter: ConfigReader[Secret] = deriveReader[Secret]
-
-    implicit val applicationConfEnvConverter: ConfigReader[ApplicationConf.Env] = (cur: ConfigCursor) => cur.asString.flatMap {
-      case "local" => Right(ApplicationConf.Local)
-      case "dev" => Right(ApplicationConf.Dev)
-      case "prod" => Right(ApplicationConf.Prod)
-      case _ => Left(ConfigReaderFailures(ConvertFailure(CannotConvert(cur.toString, "ApplicationConf.Env", "Invalid value"), cur.location, cur.path)))
-    }
-
-    implicit val sameSiteReader: ConfigReader[SameSite] = ConfigReader.fromString[SameSite](str => SameSite.parse(str).toEither(CannotConvert(str, "SameSite", s"possible values: '${SameSite.Strict.value}' or '${SameSite.Lax.value}'")))
-    implicit val authConfCookieSettingsConverter: ConfigReader[AuthConf.CookieSettings] = deriveReader[AuthConf.CookieSettings]
-    implicit val jcaSignerSettingsConverter: ConfigReader[JcaSignerSettings] = deriveReader[JcaSignerSettings]
-    implicit val jcaCrypterSettingsConverter: ConfigReader[JcaCrypterSettings] = deriveReader[JcaCrypterSettings]
-    implicit val authConfRememberMeConverter: ConfigReader[AuthConf.RememberMe] = deriveReader[AuthConf.RememberMe]
-    implicit val authConfCookieConfConverter: ConfigReader[AuthConf.CookieConf] = deriveReader[AuthConf.CookieConf]
-
-    implicit val databaseConfH2Converter: ConfigReader[DatabaseConf.H2] = deriveReader[DatabaseConf.H2]
-    implicit val databaseConfPostgreSQLConverter: ConfigReader[DatabaseConf.PostgreSQL] = deriveReader[DatabaseConf.PostgreSQL]
-
-    implicit val emailSrvConfConsoleConverter: ConfigReader[EmailSrv.Conf.Console] = deriveReader[EmailSrv.Conf.Console]
-    implicit val emailSrvConfInMemeryConverter: ConfigReader[EmailSrv.Conf.InMemery] = deriveReader[EmailSrv.Conf.InMemery]
-    implicit val emailSrvConfSendGridConverter: ConfigReader[EmailSrv.Conf.SendGrid] = deriveReader[EmailSrv.Conf.SendGrid]
-
-    implicit val applicationConfConverter: ConfigReader[ApplicationConf] = deriveReader[ApplicationConf]
-    implicit val authConfConverter: ConfigReader[AuthConf] = deriveReader[AuthConf]
-    implicit val databaseConfConverter: ConfigReader[DatabaseConf] = deriveReader[DatabaseConf]
-    implicit val appConfConverter: ConfigReader[EmailSrv.Conf] = deriveReader[EmailSrv.Conf]
-
-    implicit val emailSrvConfConverter: ConfigReader[AppConf] = deriveReader[AppConf]
-
-    pureconfig.loadConfig[AppConf](conf) match {
+    pureconfig.loadConfig[AppConf](conf)(Readers.reader) match {
       case Right(appConf) => Success(appConf)
       case Left(failures) => Failure(new IllegalArgumentException("Unable to load AppConf:\n" + failures.toList.map(format).mkString("\n")))
     }
@@ -67,6 +35,44 @@ object AppConf {
 
   private def format(f: ConfigReaderFailure): String =
     "  - " + f.description + f.location.map(" " + _.description).getOrElse("")
+
+  object Readers {
+
+    import pureconfig.generic.semiauto._
+
+    private implicit val secretReader: ConfigReader[Secret] = deriveReader[Secret]
+
+    private implicit val applicationConfEnvReader: ConfigReader[ApplicationConf.Env] = (cur: ConfigCursor) => cur.asString.flatMap {
+      case "local" => Right(ApplicationConf.Local)
+      case "dev" => Right(ApplicationConf.Dev)
+      case "prod" => Right(ApplicationConf.Prod)
+      case _ => Left(ConfigReaderFailures(ConvertFailure(CannotConvert(cur.toString, "ApplicationConf.Env", "Invalid value"), cur.location, cur.path)))
+    }
+
+    private implicit val sameSiteReader: ConfigReader[SameSite] = ConfigReader.fromString[SameSite](str => SameSite.parse(str).toEither(CannotConvert(str, "SameSite", s"possible values: '${SameSite.Strict.value}' or '${SameSite.Lax.value}'")))
+    private implicit val authConfCookieSettingsReader: ConfigReader[AuthConf.CookieSettings] = deriveReader[AuthConf.CookieSettings]
+    private implicit val jcaSignerSettingsReader: ConfigReader[JcaSignerSettings] = deriveReader[JcaSignerSettings]
+    private implicit val jcaCrypterSettingsReader: ConfigReader[JcaCrypterSettings] = deriveReader[JcaCrypterSettings]
+    private implicit val authConfRememberMeReader: ConfigReader[AuthConf.RememberMe] = deriveReader[AuthConf.RememberMe]
+    private implicit val authConfCookieConfReader: ConfigReader[AuthConf.CookieConf] = deriveReader[AuthConf.CookieConf]
+
+    private implicit val databaseConfH2Reader: ConfigReader[DatabaseConf.H2] = deriveReader[DatabaseConf.H2]
+    private implicit val databaseConfPostgreSQLReader: ConfigReader[DatabaseConf.PostgreSQL] = deriveReader[DatabaseConf.PostgreSQL]
+
+    private implicit val emailSrvConfConsoleReader: ConfigReader[EmailSrv.Conf.Console] = deriveReader[EmailSrv.Conf.Console]
+    private implicit val emailSrvConfInMemeryReader: ConfigReader[EmailSrv.Conf.InMemery] = deriveReader[EmailSrv.Conf.InMemery]
+    private implicit val emailSrvConfSendGridReader: ConfigReader[EmailSrv.Conf.SendGrid] = deriveReader[EmailSrv.Conf.SendGrid]
+
+    private implicit val applicationConfReader: ConfigReader[ApplicationConf] = deriveReader[ApplicationConf]
+    private implicit val authConfReader: ConfigReader[AuthConf] = deriveReader[AuthConf]
+    private implicit val databaseConfReader: ConfigReader[DatabaseConf] = deriveReader[DatabaseConf]
+    private implicit val emailSrvConfReader: ConfigReader[EmailSrv.Conf] = deriveReader[EmailSrv.Conf]
+
+    private implicit val appConfReader: ConfigReader[AppConf] = deriveReader[AppConf]
+
+    val reader: Derivation[ConfigReader[AppConf]] = implicitly[Derivation[ConfigReader[AppConf]]]
+  }
+
 }
 
 final case class ApplicationConf(env: ApplicationConf.Env)
