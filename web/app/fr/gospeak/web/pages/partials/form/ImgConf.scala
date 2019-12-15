34a249f5b6e5f46d9d97ca552fb9be8331371c2c
Loïc Knuchel <loicknuchel@gmail.com>
2019-12-15 12:08:09 +0100

    Improve image upload

--- web/app/fr/gospeak/web/pages/partials/form/ImgConf.scala
+++ web/app/fr/gospeak/web/pages/partials/form/ImgConf.scala
@@ -1,60 +1,72 @@
 package fr.gospeak.web.pages.partials.form
 
+import fr.gospeak.core.domain.{Group, User}
 import fr.gospeak.web.utils.{OrgaReq, UserReq}
 import play.api.mvc.AnyContent
 
 final case class ImgConf(folder: String,
                          name: Option[String],
+                         tags: Seq[String],
                          maxFiles: Option[Int],
                          ratio: Option[Double],
                          args: Seq[(String, String)])
 
 object ImgConf {
-  def userAvatar()(implicit req: UserReq[AnyContent]): ImgConf =
+  def externalCfpLogo()(implicit req: UserReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"users/${req.user.id.value}",
-      name = Some("avatar"),
+      folder = s"ext-cfps",
+      name = None,
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(1),
       args = Seq())
 
-  def externalCfpLogo(): ImgConf =
+  def userAvatar()(implicit req: UserReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"ext-cfps",
-      name = None,
+      folder = userFolder(req.user),
+      name = Some("avatar"),
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(1),
       args = Seq())
 
   def groupLogo(args: Seq[(String, String)])(implicit req: OrgaReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"groups/${req.group.id.value}",
+      folder = groupFolder(req.group),
       name = Some("logo"),
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(1),
       args = args)
 
   def groupBanner(args: Seq[(String, String)])(implicit req: OrgaReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"groups/${req.group.id.value}",
+      folder = groupFolder(req.group),
       name = Some("banner"),
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(3),
       args = args)
 
   def partnerLogo()(implicit req: OrgaReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"groups/${req.group.id.value}/partners",
+      folder = groupFolder(req.group) + "/partners",
       name = None,
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(1),
       args = Seq())
 
   def slackBotAvatar(args: Seq[(String, String)])(implicit req: OrgaReq[AnyContent]): ImgConf =
     ImgConf(
-      folder = s"groups/${req.group.id.value}",
+      folder = groupFolder(req.group),
       name = Some("slack-bot-avatar"),
+      tags = Seq(req.user.id.value, req.user.slug.value),
       maxFiles = Some(1),
       ratio = Some(1),
       args = Seq())
+
+  private def userFolder(user: User): String = s"users/${user.slug.value}/${user.id.value}"
+
+  private def groupFolder(group: Group): String = s"groups/${group.slug.value}/${group.id.value}"
 }
