4201997662cae6667b94633fdb07422167ea49a5
Loïc Knuchel <loicknuchel@gmail.com>
2019-12-15 20:54:49 +0100

    Use PROD cloudinary for some images

--- core/src/main/scala/fr/gospeak/core/domain/utils/Constants.scala
+++ core/src/main/scala/fr/gospeak/core/domain/utils/Constants.scala
@@ -8,6 +8,13 @@ object Constants {
   val defaultZoneId: ZoneId = ZoneId.of("Europe/Paris")
   val gravatarStyle = "identicon"
 
+  object Image {
+    val gospeakLogoText = "https://res.cloudinary.com/gospeak/image/upload/gospeak/logo-text.svg"
+    val placeholderGroupLogo = "https://res.cloudinary.com/gospeak/image/upload/placeholders/group-logo.png"
+    val placeholderNoVenueForEvent = "https://res.cloudinary.com/gospeak/image/upload/placeholders/no-venue-for-event.png"
+    val placeholderUnknownUser = "https://res.cloudinary.com/gospeak/image/upload/placeholders/unknown-user.png"
+  }
+
   object Contact {
     val admin: EmailAddress.Contact = EmailAddress.Contact(EmailAddress.from("contact@gospeak.io").right.get, Some("Gospeak"))
     val noReply: EmailAddress.Contact = EmailAddress.Contact(EmailAddress.from("noreply@gospeak.io").right.get, Some("Gospeak"))
