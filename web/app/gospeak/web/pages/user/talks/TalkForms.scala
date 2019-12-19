a6447e2842982bb060e694beabe2db0d4dd955b5
Loïc Knuchel <loicknuchel@gmail.com>
2019-12-19 21:26:59 +0100

    Add speaker performances

--- web/app/gospeak/web/pages/user/talks/TalkForms.scala
+++ web/app/gospeak/web/pages/user/talks/TalkForms.scala
@@ -1,9 +1,10 @@
 package gospeak.web.pages.user.talks
 
+import gospeak.core.domain.Performance
 import gospeak.core.domain.Talk
 import gospeak.web.utils.Mappings._
-import play.api.data.{Form, Mapping}
 import play.api.data.Forms._
+import play.api.data.{Form, Mapping}
 
 object TalkForms {
   val talkMappings: Mapping[Talk.Data] = mapping(
@@ -16,4 +17,12 @@ object TalkForms {
     "tags" -> tags
   )(Talk.Data.apply)(Talk.Data.unapply)
   val create: Form[Talk.Data] = Form(talkMappings)
+
+  val performanceMappings: Mapping[Performance.Data] = mapping(
+    "venue" -> nonEmptyText,
+    "title" -> nonEmptyText,
+    "description" -> markdown
+  )(Performance.Data.apply)(Performance.Data.unapply)
+  val createPerformance: Form[Performance.Data] = Form(performanceMappings)
 }
+
