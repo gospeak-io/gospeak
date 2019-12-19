fe528ca1b9c3db52a870e02ce88928ad3df1d889
Loïc Knuchel <loicknuchel@gmail.com>
2019-12-19 10:25:17 +0100

    fix

--- infra/src/main/scala/fr/gospeak/infra/services/AvatarSrv.scala
+++ infra/src/main/scala/fr/gospeak/infra/services/AvatarSrv.scala
@@ -1,5 +1,7 @@
 package fr.gospeak.infra.services
 
+import java.net.URLEncoder
+
 import fr.gospeak.core.domain.User
 import fr.gospeak.libs.scalautils.domain.Image.{AdorableUrl, GravatarUrl}
 import fr.gospeak.libs.scalautils.domain.{Avatar, EmailAddress}
@@ -8,7 +10,7 @@ class AvatarSrv {
   def getDefault(email: EmailAddress, slug: User.Slug): Avatar = {
     val size = 200
     val default = AdorableUrl(slug.value, Some(size)).value
-    val gravatar = GravatarUrl(email, Seq("size" -> s"$size", "default" -> default))
+    val gravatar = GravatarUrl(email, Seq("size" -> s"$size", "default" -> URLEncoder.encode(default, "UTF8")))
     Avatar(gravatar.toUrl)
   }
 }
