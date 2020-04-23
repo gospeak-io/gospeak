1524752474a2ecccb967720e75ef2ab1623888ca
Loïc Knuchel <loicknuchel@gmail.com>
2020-04-23 18:01:28 +0200

    Add Vimeo url parsing

--- libs/src/main/scala/gospeak/libs/scala/domain/Url.scala
+++ libs/src/main/scala/gospeak/libs/scala/domain/Url.scala
@@ -18,11 +18,25 @@ object Url {
       val url = new Url(in, parsed)
       Twitter.from(url).toOption
         .orElse(YouTube.from(url).toOption)
+        .orElse(Vimeo.from(url).toOption)
         .orElse(LinkedIn.from(url).toOption)
         .orElse(Meetup.from(url).toOption)
         .orElse(Github.from(url).toOption)
         .getOrElse(url)
     }.toEither.left.map(e => CustomException(s"'$in' is an invalid Url", Seq(CustomError(e.getMessage))))
+  }
+
+  sealed trait Video extends Url {
+    val platform: String
+
+    def videoId: String
+  }
+
+  object Video {
+    def from(in: String): Either[CustomException, Url.Video] = Url.from(in).flatMap {
+      case u: Url.Video => Right(u)
+      case _ => Left(CustomException(s"'$in' is an invalid video Url"))
+    }
   }
 
   final class Twitter private(url: Url) extends Url(url.value, url.parsed) {
@@ -57,7 +71,7 @@ object Url {
     def from(in: String): Either[CustomException, LinkedIn] = Url.from(in).flatMap(from)
   }
 
-  final class YouTube private(url: Url) extends Url(url.value, url.parsed) {
+  sealed class YouTube private(url: Url) extends Url(url.value, url.parsed) {
     override def handle: String =
       parsed.parameters.get("v")
         .orElse(parsed.parameters.get("list"))
@@ -69,13 +83,67 @@ object Url {
     private val regex1 = "https?://(?:www\\.)?youtube\\.com/.*".r
     private val regex2 = "https?://youtu\\.be/.*".r
 
-    def from(u: Url): Either[CustomException, YouTube] = u.value match {
+    def from(u: Url): Either[CustomException, YouTube] = (u.value match {
       case regex1() => Right(new YouTube(u))
       case regex2() => Right(new YouTube(u))
       case _ => Left(CustomException(s"'${u.value}' is not a YouTube url"))
-    }
+    }).map(y => Video.from(y).getOrElse(y))
 
     def from(in: String): Either[CustomException, YouTube] = Url.from(in).flatMap(from)
+
+    final class Video private(url: Url) extends YouTube(url) with Url.Video {
+      val platform: String = "YouTube"
+
+      def videoId: String = handle
+    }
+
+    object Video {
+      def from(u: YouTube): Either[CustomException, Video] =
+        if (u.parsed.path == List("watch")) Right(new Video(u))
+        else if (u.parsed.host == "youtu.be") Right(new Video(u))
+        else Left(CustomException(s"'${u.value}' is not a YouTube video url"))
+
+      def from(u: Url): Either[CustomException, Video] = YouTube.from(u).flatMap(from)
+
+      def from(u: String): Either[CustomException, Video] = YouTube.from(u).flatMap(from)
+    }
+
+  }
+
+  sealed class Vimeo private(url: Url) extends Url(url.value, url.parsed) {
+    override def handle: String = parsed.path.lastOption.getOrElse(super.handle)
+  }
+
+  object Vimeo {
+    private val regex = "https?://(?:www\\.)?vimeo\\.com.*".r
+
+    def from(u: Url): Either[CustomException, Vimeo] = (u.value match {
+      case regex() => Right(new Vimeo(u))
+      case _ => Left(CustomException(s"'${u.value}' is not a Vimeo url"))
+    }).map(v => Video.from(v).getOrElse(v))
+
+    def from(in: String): Either[CustomException, Vimeo] = Url.from(in).flatMap(from)
+
+    final class Video private(url: Url) extends Vimeo(url) with Url.Video {
+      val platform: String = "Vimeo"
+
+      def videoId: String = handle
+    }
+
+    object Video {
+      private val onlyNumbers = "[0-9]+".r
+
+      def from(u: Vimeo): Either[CustomException, Video] = u.parsed.path match {
+        case Seq(onlyNumbers()) => Right(new Video(u))
+        case _ :+ "video" :+ onlyNumbers() => Right(new Video(u))
+        case _ => Left(CustomException(s"'${u.value}' is not a Vimeo video url"))
+      }
+
+      def from(u: Url): Either[CustomException, Video] = Vimeo.from(u).flatMap(from)
+
+      def from(u: String): Either[CustomException, Video] = Vimeo.from(u).flatMap(from)
+    }
+
   }
 
   final class Meetup private(url: Url) extends Url(url.value, url.parsed) {
@@ -111,7 +179,7 @@ object Url {
   private[domain] case class ParsedUrl(protocol: String,
                                        host: String,
                                        port: Option[Int],
-                                       path: Seq[String],
+                                       path: List[String],
                                        parameters: Map[String, String],
                                        fragment: Option[String]) {
     def domain: String = host.split('.').takeRight(2).mkString(".")
@@ -125,7 +193,7 @@ object Url {
         protocol = u.getProtocol,
         host = u.getHost,
         port = Option(u.getPort).filter(_ != -1),
-        path = u.getPath.split('/').filter(_.nonEmpty),
+        path = u.getPath.split('/').filter(_.nonEmpty).toList,
         parameters = Option(u.getQuery).getOrElse("")
           .split('&')
           .filter(_.contains("="))
--- libs/src/test/scala/gospeak/libs/scala/domain/UrlSpec.scala
+++ libs/src/test/scala/gospeak/libs/scala/domain/UrlSpec.scala
@@ -17,70 +17,82 @@ class UrlSpec extends BaseSpec {
       Url.from("http://example.fr") shouldBe a[Right[_, _]]
       Url.from("http://sub.domain.ext/path/to/file?p1=value&p2=other#fragment") shouldBe a[Right[_, _]]
     }
-    describe("Twitter") {
-      it("should identify urls") {
-        Seq(
-          Url.from("https://twitter.com/gospeak_io").get -> "@gospeak_io",
-          Url.from("https://twitter.com/gospeak_io/").get -> "@gospeak_io",
-        ).foreach {
-          case (u: Url.Twitter, handle) => u.handle shouldBe handle
-          case (u, _) => fail(s"${u.value} is a Twitter url")
-        }
+    it("should identify Twitter urls") {
+      Seq(
+        Url.from("https://twitter.com/gospeak_io").get -> "@gospeak_io",
+        Url.from("https://twitter.com/gospeak_io/").get -> "@gospeak_io",
+      ).foreach {
+        case (u: Url.Twitter, handle) => u.handle shouldBe handle
+        case (u, _) => fail(s"${u.value} is a Twitter url")
       }
     }
-    describe("LinkedIn") {
-      it("should identify urls") {
-        Seq(
-          Url.from("https://www.linkedin.com/in/loicknuchel").get -> "loicknuchel",
-          Url.from("http://fr.linkedin.com/in/loicknuchel/").get -> "loicknuchel",
-          Url.from("https://www.linkedin.com/company/humantalksparis").get -> "humantalksparis",
-          Url.from("https://www.linkedin.com/company/14816213/admin").get -> "14816213",
-        ).foreach {
-          case (u: Url.LinkedIn, handle) => u.handle shouldBe handle
-          case (u, _) => fail(s"${u.value} is a LinkedIn url")
-        }
+    it("should identify LinkedIn urls") {
+      Seq(
+        Url.from("https://www.linkedin.com/in/loicknuchel").get -> "loicknuchel",
+        Url.from("http://fr.linkedin.com/in/loicknuchel/").get -> "loicknuchel",
+        Url.from("https://www.linkedin.com/company/humantalksparis").get -> "humantalksparis",
+        Url.from("https://www.linkedin.com/company/14816213/admin").get -> "14816213",
+      ).foreach {
+        case (u: Url.LinkedIn, handle) => u.handle shouldBe handle
+        case (u, _) => fail(s"${u.value} is a LinkedIn url")
       }
     }
-    describe("YouTube") {
-      it("should identify urls") {
-        Seq(
-          Url.from("https://www.youtube.com/c/HumanTalksParis").get -> "HumanTalksParis",
-          Url.from("https://www.youtube.com/channel/UCKFAwlgWiAB4vUpgnS63qog").get -> "UCKFAwlgWiAB4vUpgnS63qog",
-          Url.from("https://www.youtube.com/channel/UCKFAwlgWiAB4vUpgnS63qog/videos").get -> "UCKFAwlgWiAB4vUpgnS63qog",
-          Url.from("https://www.youtube.com/playlist?list=PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx").get -> "PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx",
-          Url.from("https://www.youtube.com/watch?v=QfmVc9c_8Po&list=PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx").get -> "QfmVc9c_8Po",
-          Url.from("https://www.youtube.com/watch?v=QfmVc9c_8Po").get -> "QfmVc9c_8Po",
-          Url.from("https://youtu.be/QfmVc9c_8Po").get -> "QfmVc9c_8Po",
-        ).foreach {
-          case (u: Url.YouTube, handle) => u.handle shouldBe handle
-          case (u, _) => fail(s"${u.value} is a YouTube url")
-        }
+    it("should identify YouTube urls") {
+      Seq(
+        (Url.from("https://loicknuchel.fr").get, None, false),
+        (Url.from("https://www.youtube.com/c/HumanTalksParis").get, Some("HumanTalksParis"), false),
+        (Url.from("https://www.youtube.com/channel/UCKFAwlgWiAB4vUpgnS63qog").get, Some("UCKFAwlgWiAB4vUpgnS63qog"), false),
+        (Url.from("https://www.youtube.com/channel/UCKFAwlgWiAB4vUpgnS63qog/videos").get, Some("UCKFAwlgWiAB4vUpgnS63qog"), false),
+        (Url.from("https://www.youtube.com/playlist?list=PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx").get, Some("PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx"), false),
+        (Url.from("https://www.youtube.com/watch?v=QfmVc9c_8Po&list=PLjkHSzY9VuL96Z9gpNrlwztU3w72zccCx").get, Some("QfmVc9c_8Po"), true),
+        (Url.from("https://www.youtube.com/watch?v=QfmVc9c_8Po").get, Some("QfmVc9c_8Po"), true),
+        (Url.from("https://youtu.be/QfmVc9c_8Po").get, Some("QfmVc9c_8Po"), true),
+      ).foreach {
+        case (u: Url.YouTube.Video, handle, isVideo) =>
+          if (isVideo) Some(u.videoId) shouldBe handle else fail(s"${u.value} is not a YouTube video url")
+        case (u: Url.YouTube, handle, isVideo) =>
+          if (isVideo) fail(s"${u.value} is a YouTube video url") else Some(u.handle) shouldBe handle
+        case (u, handle, _) =>
+          if (handle.nonEmpty) fail(s"${u.value} is a YouTube url")
       }
     }
-    describe("Meetup") {
-      it("should identify urls") {
-        Seq(
-          Url.from("https://www.meetup.com/HumanTalks-Paris").get -> "HumanTalks-Paris",
-          Url.from("https://www.meetup.com/HumanTalks-Paris/events/269180953/").get -> "269180953",
-          Url.from("https://www.meetup.com/members/14321102").get -> "14321102",
-          Url.from("https://www.meetup.com/fr-FR/members/14321102/").get -> "14321102",
-          Url.from("https://www.meetup.com/fr-FR/members/14321102/?op=&memberId=14321102/").get -> "14321102",
-        ).foreach {
-          case (u: Url.Meetup, handle) => u.handle shouldBe handle
-          case (u, _) => fail(s"${u.value} is a Meetup url")
-        }
+    it("should identify Vimeo urls") {
+      Seq(
+        (Url.from("https://loicknuchel.fr").get, None, false),
+        (Url.from("https://vimeo.com/parisweb").get, Some("parisweb"), false),
+        (Url.from("https://vimeo.com/showcase/6597308").get, Some("6597308"), false),
+        (Url.from("https://vimeo.com/showcase/6597308/video/380320538").get, Some("380320538"), true),
+        (Url.from("https://vimeo.com/380320538").get, Some("380320538"), true),
+        (Url.from("https://vimeo.com/380320538#t=10s").get, Some("380320538"), true)
+      ).foreach {
+        case (u: Url.Vimeo.Video, handle, isVideo) =>
+          if (isVideo) Some(u.videoId) shouldBe handle else fail(s"${u.value} is not a Vimeo video url")
+        case (u: Url.Vimeo, handle, isVideo) =>
+          if (isVideo) fail(s"${u.value} is a Vimeo video url") else Some(u.handle) shouldBe handle
+        case (u, handle, _) =>
+          if (handle.nonEmpty) fail(s"${u.value} is a Vimeo url")
       }
     }
-    describe("Github") {
-      it("should identify urls") {
-        Seq(
-          Url.from("https://github.com/gospeak-io").get -> "gospeak-io",
-          Url.from("https://github.com/gospeak-io/").get -> "gospeak-io",
-          Url.from("https://github.com/gospeak-io/gospeak").get -> "gospeak",
-        ).foreach {
-          case (u: Url.Github, handle) => u.handle shouldBe handle
-          case (u, _) => fail(s"${u.value} is a Github url")
-        }
+    it("should identify Meetup urls") {
+      Seq(
+        Url.from("https://www.meetup.com/HumanTalks-Paris").get -> "HumanTalks-Paris",
+        Url.from("https://www.meetup.com/HumanTalks-Paris/events/269180953/").get -> "269180953",
+        Url.from("https://www.meetup.com/members/14321102").get -> "14321102",
+        Url.from("https://www.meetup.com/fr-FR/members/14321102/").get -> "14321102",
+        Url.from("https://www.meetup.com/fr-FR/members/14321102/?op=&memberId=14321102/").get -> "14321102",
+      ).foreach {
+        case (u: Url.Meetup, handle) => u.handle shouldBe handle
+        case (u, _) => fail(s"${u.value} is a Meetup url")
+      }
+    }
+    it("should identify Github urls") {
+      Seq(
+        Url.from("https://github.com/gospeak-io").get -> "gospeak-io",
+        Url.from("https://github.com/gospeak-io/").get -> "gospeak-io",
+        Url.from("https://github.com/gospeak-io/gospeak").get -> "gospeak",
+      ).foreach {
+        case (u: Url.Github, handle) => u.handle shouldBe handle
+        case (u, _) => fail(s"${u.value} is a Github url")
       }
     }
     describe("ParsedUrl") {
@@ -89,30 +101,30 @@ class UrlSpec extends BaseSpec {
           protocol = "https",
           host = "loicknuchel.fr",
           port = None,
-          path = Seq(),
+          path = List(),
           parameters = Map(),
           fragment = None))
         ParsedUrl.from("http://www.loicknuchel.fr:8080/blog/test.html?page=1&test=true#header") shouldBe Success(ParsedUrl(
           protocol = "http",
           host = "www.loicknuchel.fr",
           port = Some(8080),
-          path = Seq("blog", "test.html"),
+          path = List("blog", "test.html"),
           parameters = Map("page" -> "1", "test" -> "true"),
           fragment = Some("header")))
       }
       it("should compute domain") {
-        val url = ParsedUrl(protocol = "https", host = "", port = None, path = Seq(), parameters = Map(), fragment = None)
+        val url = ParsedUrl(protocol = "https", host = "", port = None, path = List(), parameters = Map(), fragment = None)
         url.domain shouldBe ""
         url.copy(host = "loicknuchel.fr").domain shouldBe "loicknuchel.fr"
         url.copy(host = "blog.loicknuchel.fr").domain shouldBe "loicknuchel.fr"
         url.copy(host = "a.b.c.loicknuchel.fr").domain shouldBe "loicknuchel.fr"
       }
       it("should compute file") {
-        val url = ParsedUrl(protocol = "https", host = "loicknuchel.fr", port = None, path = Seq(), parameters = Map(), fragment = None)
+        val url = ParsedUrl(protocol = "https", host = "loicknuchel.fr", port = None, path = List(), parameters = Map(), fragment = None)
         url.file shouldBe None
-        url.copy(path = Seq("blog")).file shouldBe None
-        url.copy(path = Seq("index.html")).file shouldBe Some("index.html")
-        url.copy(path = Seq("blog", "index.html")).file shouldBe Some("index.html")
+        url.copy(path = List("blog")).file shouldBe None
+        url.copy(path = List("index.html")).file shouldBe Some("index.html")
+        url.copy(path = List("blog", "index.html")).file shouldBe Some("index.html")
       }
     }
   }
