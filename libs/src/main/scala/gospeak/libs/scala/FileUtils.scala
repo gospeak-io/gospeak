ef819b1616bbd668c9de47e452eb1d5db43cb001
Loïc Knuchel <loicknuchel@gmail.com>
2020-08-25 19:46:34 +0200

    Improve FileUtils

--- libs/src/main/scala/gospeak/libs/scala/FileUtils.scala
+++ libs/src/main/scala/gospeak/libs/scala/FileUtils.scala
@@ -1,5 +1,6 @@
 package gospeak.libs.scala
 
+import java.io.File
 import java.nio.file.{Files, Paths}
 
 import scala.collection.JavaConverters._
@@ -21,8 +22,31 @@ object FileUtils {
   def read(path: String): Try[String] =
     Try(Files.readAllLines(Paths.get(path))).map(_.asScala.mkString("\n"))
 
-  def delete(path: String): Try[Unit] =
+  def listFiles(path: String, recursively: Boolean = true): Try[List[String]] = Try {
+    def listDir(dir: File): List[File] = {
+      if (dir.isDirectory) {
+        Option(dir.listFiles).map(_.toList.flatMap(f => if (recursively) listDir(f) else List(f))).getOrElse(List())
+      } else {
+        List(dir)
+      }
+    }
+
+    listDir(new File(path)).filter(_.isFile).map(_.getPath).sorted
+  }
+
+  def deleteFile(path: String): Try[Unit] =
     Try(Files.delete(Paths.get(path)))
+
+  def delete(path: String): Try[Unit] = Try {
+    def deleteDir(dir: File): Boolean = {
+      if (dir.isDirectory) {
+        Option(dir.listFiles).foreach(_.foreach(deleteDir))
+      }
+      dir.delete
+    }
+
+    deleteDir(new File(path))
+  }
 
   // remove the first folder if it does not exist as IntelliJ uses project home and sbt uses module home to run tests :(
   def adaptLocalPath(path: String): String = {
