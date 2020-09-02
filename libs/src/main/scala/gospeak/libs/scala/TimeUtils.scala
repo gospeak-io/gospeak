0dc7293b3ecdb8b13c84eb9b57a8fc2b844c9283
Loïc Knuchel <loicknuchel@gmail.com>
2020-09-02 17:17:36 +0200

    Improve typed sql dsl

--- libs/src/main/scala/gospeak/libs/scala/TimeUtils.scala
+++ libs/src/main/scala/gospeak/libs/scala/TimeUtils.scala
@@ -1,13 +1,16 @@
 package gospeak.libs.scala
 
-import java.time.{Duration, Instant, LocalDateTime, ZoneId}
+import java.time._
 
 import scala.concurrent.duration.{FiniteDuration, MILLISECONDS, NANOSECONDS}
 import scala.util.Try
 
 object TimeUtils {
-  def toInstant(date: LocalDateTime, zone: ZoneId): Instant =
+  def toInstant(date: LocalDateTime, zone: ZoneId = ZoneOffset.UTC): Instant =
     date.toInstant(zone.getRules.getOffset(date))
+
+  def toLocalDateTime(i: Instant, zone: ZoneId = ZoneOffset.UTC): LocalDateTime =
+    LocalDateTime.ofInstant(i, zone)
 
   def toFiniteDuration(duration: String): Try[FiniteDuration] =
     Try(Duration.parse(duration)).map(v => FiniteDuration(v.toNanos, NANOSECONDS).toCoarsest)
